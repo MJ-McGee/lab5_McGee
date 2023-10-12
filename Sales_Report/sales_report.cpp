832763f4687c6218da45c028fe7f5c6c06fcaca6
MJ McGee <mj.mcgee@ku.edu>
2023-10-12 16:20:33 -0500

    Added file folders

---
+++ Sales_Report/sales_report.cpp
@@ -0,0 +1,22 @@
+/*
+Author: MJ McGee
+Date Created: 10/12/23
+Date Last Modified: 10/12/23
+Description: Read in a file of sales data and process it into a sales report
+*/
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+int main() {
+
+	ifstream file("sales_data.txt");
+
+	char *data;
+	file.read(data, 20);
+
+
+	file.close();
+	return 0;
+}
